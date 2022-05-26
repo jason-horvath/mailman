8221511c3d156aa2479c8434a298841484758012
Jason Horvath <kaingnx@gmail.com>
2022-05-26 00:16:38 -0400

    add cors and routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const express = require('express')
+const cors = require('cors')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const { corsProtector } = require('./middleware/httpClientMiddleware')
+const app = express()
 
+app.use(cors(corsProtector))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use('/api/email', require('./routes/mailRoutes'))
+app.use(errorHandler)
 
+app.listen(port)
